6ee9c46bd7f5583b413647175b85baa03b3653b7
Mart337i <mart337i@gmail.com>
2023-04-26 14:24:49 +0200

    Implement default repository pattern

---
+++ Vesuvius-MDT/IRepository/IGenericRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+
+namespace Vesuvius_MDT.IRepository;
+
+public interface IGenericRepository<TEntity> where TEntity : class
+{
+    TEntity? GetById(int id);
+    IEnumerable<TEntity> GetAll();
+    IEnumerable<TEntity> Find(Expression<Func<TEntity, bool>> expression);
+    void Add(TEntity entity);
+    void AddRange(IEnumerable<TEntity> entities);
+    void Remove(TEntity entity);
+    void RemoveRange(IEnumerable<TEntity> entities);
+}
---
+++ Vesuvius-MDT/Repository/GenericRepository.cs
@@ -0,0 +1,46 @@
+﻿using System.Linq.Expressions;
+using Vesuvius_MDT.Data;
+using Vesuvius_MDT.IRepository;
+
+namespace Vesuvius_MDT.Repository;
+
+public class GenericRepository<T> : IGenericRepository<T> where T : class
+{
+    internal readonly DataContext _context;
+    
+    //Dependency Injection
+    public GenericRepository(DataContext context)
+    {
+        _context = context;
+    }
+
+    public void Add(T entity)
+    {
+        _context.Set<T>().Add(entity);
+    }
+
+    public void AddRange(IEnumerable<T> entities)
+    {
+        _context.Set<T>().AddRange(entities);
+    }
+    public IEnumerable<T> Find(Expression<Func<T, bool>> expression)
+    {
+        return _context.Set<T>().Where(expression);
+    }
+    public IEnumerable<T> GetAll()
+    {
+        return _context.Set<T>().ToList();
+    }
+    public T? GetById(int id)
+    {
+        return _context.Set<T>().Find(id);
+    }
+    public void Remove(T entity)
+    {
+        _context.Set<T>().Remove(entity);
+    }
+    public void RemoveRange(IEnumerable<T> entities)
+    {
+        _context.Set<T>().RemoveRange(entities);
+    }
+}
