3f7d863e56f6d85d9fd80d812ee48c00ddafecb6
Mart337i <mart337i@gmail.com>
2023-04-27 09:00:32 +0200

    [ADD] Added comment explaining the use of the  repository pattern

--- Vesuvius-MDT/IRepository/IGenericRepository.cs
+++ Vesuvius-MDT/IRepository/IGenericRepository.cs
@@ -3,6 +3,17 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Vesuvius_MDT.IRepository;
 
+/* This projects use of repository pattern.
+ * We have chosen to build one interface and one implementation of it called "GenericRepository"
+ * from where we will inherit from.
+ *
+ * This is done against the best practice of an interface foreach repository
+ * In this case it adds unwanted complexity.
+ *
+ * Official doc:
+ * https://learn.microsoft.com/en-us/aspnet/mvc/overview/older-versions/getting-started-with-ef-5-using-mvc-4/implementing-the-repository-and-unit-of-work-patterns-in-an-asp-net-mvc-application
+ */
+
 public interface IGenericRepository<TEntity> where TEntity : class
 {
     TEntity? GetById(int id);
