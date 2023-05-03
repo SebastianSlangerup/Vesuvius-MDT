f6aeab3525c8bcdb2458fa317ed5ed2f4a3f30a3
Mart337i <mart337i@gmail.com>
2023-05-03 09:24:25 +0200

    [ADD] Add crud to controllers

--- Vesuvius-MDT/Controllers/MenuItemController.cs
+++ Vesuvius-MDT/Controllers/MenuItemController.cs
@@ -1,3 +1,4 @@
+using System.Data;
 using Microsoft.AspNetCore.Mvc;
 using Vesuvius_MDT.Data;
 using Vesuvius_MDT.Models;
@@ -21,5 +22,87 @@ public class MenuItemController : Controller
         var menuItems = _unitOfWork.MenuItemRepository.GetAll();
         
         return Ok(menuItems);
+    }
+
+    [HttpGet("menu-item/{id:int}]")]
+    public ActionResult<MenuItem> Get(int id)
+    {
+        MenuItem menuItem = _unitOfWork.MenuItemRepository.GetById(id);
+
+        if (menuItem == null)
+        {
+            return NotFound();
+        }
+        
+        _unitOfWork.Save();
+        return Ok(menuItem);
+
+    }
+
+    [HttpPost("/menu-item/new")]
+    public ActionResult<MenuItem> Add(MenuItem menuItem)
+    {
+        try
+        {
+            _unitOfWork.MenuItemRepository.Add(menuItem);
+            _unitOfWork.Save();
+            return Ok(menuItem);
+        }
+        catch (DataException e)
+        {
+            Console.WriteLine(e);
+            return StatusCode(StatusCodes.Status500InternalServerError);
+        }
+    }
+
+    [HttpPut("/menu-item/{id:int}")]
+    public ActionResult<MenuItem> Update(int id, MenuItem menuItemRequest)
+    {
+        MenuItem menuItem = _unitOfWork.MenuItemRepository.GetById(id);
+
+        if (menuItem == null)
+        {
+            return NotFound();
+        }
+
+        try
+        {
+            menuItem.MenuItemId = menuItemRequest.MenuItemId;
+            menuItem.OrderItems = menuItemRequest.OrderItems;
+            menuItem.Description = menuItemRequest.Description;
+            menuItem.Price = menuItemRequest.Price;
+            menuItem.InStock = menuItemRequest.InStock;
+            menuItem.Name = menuItemRequest.Name;
+            
+            _unitOfWork.Save();
+            return Ok(menuItem);
+        }
+        catch (DataException e)
+        {
+            Console.WriteLine(e);
+            return StatusCode(StatusCodes.Status500InternalServerError);
+        }
+    }
+
+    [HttpDelete("/menu-item/{id:int}")]
+    public ActionResult Delete(int id)
+    {
+        var menu_item = _unitOfWork.MenuItemRepository.GetById(id);
+        if (menu_item == null)
+        {
+            return NotFound();
+        }
+
+        try
+        {
+            _unitOfWork.MenuItemRepository.Remove(menu_item);
+            _unitOfWork.Save();
+            return Ok();
+        }
+        catch (DataException e)
+        {
+            Console.WriteLine(e);
+            return StatusCode(StatusCodes.Status500InternalServerError);
+        }
     }
 }
--- Vesuvius-MDT/Controllers/OrderItem.cs
+++ Vesuvius-MDT/Controllers/OrderItem.cs
@@ -1,3 +1,4 @@
+using System.Data;
 using Microsoft.AspNetCore.Mvc;
 using Vesuvius_MDT.Data;
 using Vesuvius_MDT.Models;
@@ -21,5 +22,74 @@ public class OrderItemController : Controller
         var orderItems = _unitOfWork.OrderItemRepository.GetAll();
         
         return Ok(orderItems);
+    }
+
+    [HttpGet("order-items")]
+    public ActionResult<OrderItem> Add(OrderItem orderItem)
+    {
+        try
+        {
+            _unitOfWork.OrderItemRepository.Add(orderItem);
+            _unitOfWork.Save();
+            return Ok(orderItem);
+        }
+        catch (DataException e)
+        {
+            Console.WriteLine(e);
+            return StatusCode(StatusCodes.Status500InternalServerError);
+        }
+    }
+
+    [HttpPut("/order-item/{id:int}")]
+    public ActionResult<OrderItem> Update(int id, OrderItem orderRequestItem)
+    {
+        var Order_item = _unitOfWork.OrderItemRepository.GetById(id);
+        if (Order_item == null)
+        {
+            return NotFound();
+        }
+
+        try
+        {
+            Order_item.OrderId = orderRequestItem.OrderId;
+            Order_item.Count = orderRequestItem.Count;
+            Order_item.Discount = orderRequestItem.Discount;
+            Order_item.Paid = orderRequestItem.Paid;
+            Order_item.FoodStatus = orderRequestItem.FoodStatus;
+            Order_item.AddonLinks = orderRequestItem.AddonLinks;
+            Order_item.FoodStatusId = orderRequestItem.FoodStatusId;
+            Order_item.MenuItem = orderRequestItem.MenuItem;
+            Order_item.MenuItemId = orderRequestItem.MenuItemId;
+            
+            _unitOfWork.Save();
+            return Ok();
+        }
+        catch (DataException e)
+        {
+            Console.WriteLine(e);
+            return StatusCode(StatusCodes.Status500InternalServerError);
+        }
+    }
+
+    [HttpDelete("/order-item/{id:int}")]
+    public ActionResult Delete(int id)
+    {
+        var order_item = _unitOfWork.OrderItemRepository.GetById(id);
+        if (order_item == null)
+        {
+            return NotFound();
+        }
+
+        try
+        {
+            _unitOfWork.OrderItemRepository.Remove(order_item);
+            _unitOfWork.Save();
+            return Ok();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            return StatusCode(StatusCodes.Status500InternalServerError);
+        }
     }
 }
