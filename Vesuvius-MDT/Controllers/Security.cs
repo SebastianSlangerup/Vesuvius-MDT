86843b03f86e1ecc69b287cb370f9d7c8d287963
Mart337i <mart337i@gmail.com>
2023-05-08 13:27:52 +0200

    Implement JWT

---
+++ Vesuvius-MDT/Controllers/Security.cs
@@ -0,0 +1,95 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using Vesuvius_MDT.Models;
+using Vesuvius_MDT.UnitOfWorkNamespace;
+using JwtRegisteredClaimNames = Microsoft.IdentityModel.JsonWebTokens.JwtRegisteredClaimNames;
+
+namespace Vesuvius_MDT.Controllers;
+
+[ApiController]
+public class Security : Controller
+{
+    private IConfiguration _configuration;
+    public Security(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    [AllowAnonymous]
+    [HttpPost("/security/createToken")]
+    public ActionResult<string> createToken(Login login, string username, string password)
+    {
+        if (login.Username == username && login.Password == password)
+        {
+            var issuer = _configuration["Jwt:Issuer"];
+            var audience = _configuration["Jwt:Audience"];
+            var key = Encoding.ASCII.GetBytes
+                (_configuration["Jwt:Key"]);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[]
+                {
+                    new Claim("Id", Guid.NewGuid().ToString()),
+                    new Claim(JwtRegisteredClaimNames.Sub, login.Username),
+                    new Claim(JwtRegisteredClaimNames.Email, login.Username),
+                    new Claim(JwtRegisteredClaimNames.Jti,
+                        Guid.NewGuid().ToString())
+                }),
+                Expires = DateTime.UtcNow.AddMinutes(5),
+                Issuer = issuer,
+                Audience = audience,
+                SigningCredentials = new SigningCredentials
+                (new SymmetricSecurityKey(key),
+                    SecurityAlgorithms.HmacSha512Signature)
+            };
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            var jwtToken = tokenHandler.WriteToken(token);
+            string? stringToken = tokenHandler.WriteToken(token);
+            return Ok(stringToken);
+        }
+        return StatusCode(StatusCodes.Status401Unauthorized);
+    }
+
+    [AllowAnonymous]
+    [HttpPost("/security/verifyToken")]
+    public bool verifyToken(string token)
+    {
+        var validationParameters = new TokenValidationParameters()
+        {
+            IssuerSigningKey = new SymmetricSecurityKey
+                (Encoding.UTF8.GetBytes(_configuration["Jwt:Key"])),
+            ValidAudience = _configuration["Jwt:Audience"],
+            ValidIssuer = _configuration["Jwt:Issuer"],
+            ValidateLifetime = true,
+            ValidateAudience = true,
+            ValidateIssuer = true,
+            ValidateIssuerSigningKey = true
+        };
+        
+        var tokenHandler = new JwtSecurityTokenHandler();
+        SecurityToken validatedToken = null;
+
+        tokenHandler.ValidateToken(token, validationParameters, out validatedToken);
+
+        try
+        {
+            tokenHandler.ValidateToken(token, validationParameters, out validatedToken);
+        }
+        catch(SecurityTokenException)
+        {
+            return false; 
+        }
+        catch(Exception e)
+        { 
+            Console.WriteLine(e.ToString()); //something else happened
+            throw;
+        }
+        //... manual validations return false if anything untoward is discovered
+        return validatedToken != null;
+    }
+}
--- Vesuvius-MDT/Program.cs
+++ Vesuvius-MDT/Program.cs
@@ -1,3 +1,8 @@
+using System.Security;
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.IdentityModel.Tokens;
 using Vesuvius_MDT.Data;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -11,7 +16,33 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<DataContext>();
 
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(o =>
+{
+    o.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey
+            (Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"] ?? throw new SecurityException("The key set in appsetting.json is invalid"))),
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = false,
+        ValidateIssuerSigningKey = true
+    };
+});
+
+builder.Services.AddAuthorization();
+
+
 var app = builder.Build();
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
